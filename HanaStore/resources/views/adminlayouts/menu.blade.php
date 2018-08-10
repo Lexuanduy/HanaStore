391273fc56bed0b955edd6283eab220e6db46d09
trungdo1410 <trungdqd00630@fpt.edu.vn>
2018-08-10 12:36:19 +0700

    add layout to admin page include master layout,menu and nav

---
+++ HanaStore/resources/views/adminlayouts/menu.blade.php
@@ -0,0 +1,33 @@
+<div class="navbar-default sidebar" role="navigation">
+<div class="sidebar-nav navbar-collapse">
+<ul class="nav" id="side-menu">
+<li class="sidebar-search">
+<div class="input-group custom-search-form">
+<input type="text" class="form-control" placeholder="Search...">
+<span class="input-group-btn">
+<button class="btn btn-default" type="button">
+<i class="fa fa-search"></i>
+</button>
+</span>
+</div>
+<!-- /input-group -->
+</li>
+<li>
+<a href="#"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+</li>
+<li>
+<a href="#"><i class="fa fa-cube fa-fw"></i> Product<span class="fa arrow"></span></a>
+<ul class="nav nav-second-level">
+<li>
+<a href="admin/sanpham/list">List Product</a>
+</li>
+<li>
+<a href="admin/sanpham/them">Add Product</a>
+</li>
+</ul>
+<!-- /.nav-second-level -->
+</li>
+</ul>
+</div>
+<!-- /.sidebar-collapse -->
+</div>
