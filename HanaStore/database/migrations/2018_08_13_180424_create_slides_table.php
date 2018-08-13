9237e0a30d6d472b7cffac2d94c6d4a04c175b1b
SlowV <31509114+SlowV@users.noreply.github.com>
2018-08-14 01:19:10 +0700

    Đã xong migrate, model và controller cho slide

---
+++ HanaStore/database/migrations/2018_08_13_180424_create_slides_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('images');
+            $table->string('link');
+            $table->timestamps();
+            $table->integer('status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
