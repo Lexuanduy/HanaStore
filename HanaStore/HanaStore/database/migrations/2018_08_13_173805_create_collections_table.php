021a8b60633b4e553894bbf37c03c13ee63cbb97
Lexuanduy <lexuanduy07@gmail.com>
2018-08-15 14:05:14 +0700

    by duyle

---
+++ HanaStore/HanaStore/database/migrations/2018_08_13_173805_create_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCollectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collections', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('images');
+            $table->timestamps();
+            $table->integer('status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+}
