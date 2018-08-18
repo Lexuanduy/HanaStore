233207d865ff16ae78b57824a036f2707954fef8
phuocding <phuocding@gmail.com>
2018-08-18 15:48:32 +0700

    feature#5.1-create-edit-delete-products-by-phuocding add 404 view and comment in web config

--- HanaStore/routes/web.php
+++ HanaStore/routes/web.php
@@ -12,11 +12,15 @@
 */
 //test view form create layout
 
+// config web by Phuocding
 Route::resource('/admin/product', 'ProductController');
 
+// modified routing in view master
 Route::get('/admin/', function () {
     return view('admin.layout.master');
 });
+
+// don't need to show this url
 Route::get('/admin/layout/master', function () {
     return view('admin.layout.master');
 });
