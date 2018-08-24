6d23c251f69804dcd3a3f1e1216901e0a0bfac3b
Lexuanduy <lexuanduy07@gmail.com>
2018-08-24 15:18:07 +0700

    migration database

--- HanaStore/app/Product.php
+++ HanaStore/app/Product.php
@@ -17,5 +17,57 @@ class Product extends Model
         return $this->belongsTo('App\Collection','collectionId');
     }
 
-    
+    public function getDiscountPriceWithFormatAttribute()
+    {
+        if ($this->discount == 0) {
+            return sprintf('%s', number_format($this->price, 0));
+        }
+        return sprintf('%s', number_format(($this->price - ($this->price * $this->discount / 100)), 0));
+    }
+
+    public function getDiscountPriceStringAttribute()
+    {
+        if ($this->discount == 0) {
+            return sprintf('%s (vnd)', number_format($this->price, 0));
+        }
+        return sprintf('%s (vnd)', number_format(($this->price - ($this->price * $this->discount / 100)), 0));
+    }
+
+    public function getOriginalPriceStringAttribute()
+    {
+        if ($this->discount == 0) {
+            return '';
+        }
+        return sprintf('%s (vnd)', number_format($this->price, 0));
+    }
+
+    public function getDiscountPriceAttribute()
+    {
+        return $this->price - ($this->price * $this->discount / 100);
+    }
+
+    public function isNew()
+    {
+        // sản phẩm được tạo trong vòng bảy ngày thì là sản phẩm mới.
+        return time() - (60 * 60 * 24 * 7) < strtotime($this->created_at);
+    }
+
+    public function isDiscount()
+    {
+        // sản phẩm được tạo trong vòng bảy ngày thì là sản phẩm mới.
+        return $this->discount > 0;
+    }
+
+    public function getBlockStyleAttribute()
+    {
+        if ($this->isNew() && $this->isDiscount()) {
+            return ' block2-labelnewsale';
+        } else if ($this->isNew()) {
+            return ' block2-labelnew';
+        } else if ($this->isDiscount()) {
+            return ' block2-labelsale';
+        } else {
+            return '';
+        }
+    }
 }
--- HanaStore/database/migrations/2018_08_09_123712_create_products_table.php
+++ HanaStore/database/migrations/2018_08_09_123712_create_products_table.php
@@ -15,16 +15,18 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
             $table->unsignedInteger('categoryId');
             $table->foreign('categoryId')->references('id')->on('categories');
             $table->unsignedInteger('collectionId');
             $table->foreign('collectionId')->references('id')->on('collections');
-            $table->double('price');
+            $table->string('name', 50);
             $table->string('images');
-            $table->unsignedInteger('sale');
-            $table->string('description');
+            $table->integer('price');
+            $table->integer('discount');
+            $table->string('description', 250);
             $table->text('detail');
+            $table->integer('colors'); // 1. red | 2.yellow | 3.green | 4.blue | 5.white | 6.black
+            $table->integer('sizes'); // 1. XS | 2.S | 3.M | 4.L | 5.XL | 6.XXL
             $table->timestamps();
             $table->integer('status')->default(1);
         });
