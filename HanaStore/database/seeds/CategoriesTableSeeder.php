a5d869679fdcba62e0b2cee4cae29d23e802d2d9
Lexuanduy <lexuanduy07@gmail.com>
2018-08-25 17:28:52 +0700

    update migrations and test

--- HanaStore/database/seeds/CategoriesTableSeeder.php
+++ HanaStore/database/seeds/CategoriesTableSeeder.php
@@ -19,36 +19,6 @@ class CategoriesTableSeeder extends Seeder
                 'name'=>'Hoa sinh nhật',
                 'images'=>'',
                 'description'=>'Hoa được dùng để tặng trong tiệc sinh nhật'
-            ],
-            [
-                'id'=>2,
-                'name'=>'Hoa khai trương',
-                'description'=>'Hoa được dùng để trang trí rong dịp khai trương'
-            ],
-            [
-                'id'=>3,
-                'name'=>'Hoa giỏ',
-                'description'=>'Hoa được làm và trang trí thành từng giỏ để tặng hoặc trưng bày'
-            ],
-            [
-                'id'=>4,
-                'name'=>'Hoa nhập khẩu',
-                'description'=>'Hoa được nhập khẩu từ các quốc gia trên thế giới'
-            ],
-            [
-                'id'=>5,
-                'name'=>'Hoa bó',
-                'description'=>'Hoa được làm theo bó'
-            ],
-            [
-                'id'=>6,
-                'name'=>'Hoa cưới',
-                'description'=>'Hoa được làm trang trí đám cưới, bày bàn, hoa đón dâu'
-            ],
-            [
-                'id'=>7,
-                'name'=>'Hoa chúc mừng',
-                'description'=>'Hoa dùng để tặng, chúc mừng'
             ]
         ]);
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
