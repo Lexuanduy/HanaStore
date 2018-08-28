5c5b2f26c2a56fb245829e05765e84c02049563e
Your Name <you@example.com>
2018-08-28 09:12:49 +0700

    error view

---
+++ HanaStore/resources/views/admin/error/400.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="font-weight-bold" style="font-size: 50px ; text-align: center">400 Bad Request</h1>
+<p style="font-size: 20px ; text-align: center" >Your request contains bad syntax and cannot be fulfilled.</p>
+<hr>
+<h3 style="text-align: center"><a href="http://admin">Go to homepage</a></h3>
+</body>
+</html>
