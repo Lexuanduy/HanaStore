28952eb0cb09fc83566f5b26c37f08e3cf31e16c
Your Name <you@example.com>
2018-09-05 12:01:19 +0700

    Create view contact

--- HanaStore/public/js/map-custom.js
+++ HanaStore/public/js/map-custom.js
@@ -15,8 +15,8 @@
                 img_pin = 'images/icons/location.png';
             }
             if (data_map_x == null || data_map_y == null) {
-                data_map_x = 40.007749;
-                data_map_y = -93.266572;
+                data_map_x = 21.028849;
+                data_map_y = 105.781734;
             }
             if (scrollwhell == null) {
                 scrollwhell = 0;
