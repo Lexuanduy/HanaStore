d68e1a92124e56bd846c97651f728cf6fcbe3258
phuocding <phuocding@gmail.com>
2018-09-06 20:31:28 +0700

    feature#19.3-create-order-management-admin-by-phuocding solved all status order

--- HanaStore/resources/views/admin/order/list.blade.php
+++ HanaStore/resources/views/admin/order/list.blade.php
@@ -26,6 +26,17 @@
                     </div>
                 </div>
 
+                <div class="row">
+                    <form action="/admin/product">
+                        <div class="form-group col-sm-3">
+                            <div id="reportrange" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc; width: 100%">
+                                <i class="fa fa-calendar"></i>&nbsp;
+                                <span></span> <i class="fa fa-caret-down"></i>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
                 <!--List Flower Table-->
                 <div class="row" style="padding-top: 3em;">
                     <div class="panel panel-default">
@@ -97,4 +108,32 @@
         </div>
         <!--  end card  -->
     </div>
+
+    <script type="text/javascript">
+        $(function() {
+
+            var start = moment().subtract(29, 'days');
+            var end = moment();
+
+            function cb(start, end) {
+                $('#reportrange span').html(start.format('DD-MM-YYYY') + ' - ' + end.format('DD-MM-YYYY'));
+            }
+
+            $('#reportrange').daterangepicker({
+                startDate: start,
+                endDate: end,
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                }
+            }, cb);
+
+            cb(start, end);
+
+        });
+    </script>
 @endsection
